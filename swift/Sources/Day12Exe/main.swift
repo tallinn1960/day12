ff42fa443d962dee91a1bdc85d11ff38834cca4d
Olaf Schlüter <tallinn1960@yahoo.de>
2024-07-02 18:42:12 +0200

    swift exe takes input as argument

--- swift/Sources/Day12Exe/main.swift
+++ swift/Sources/Day12Exe/main.swift
@@ -8,7 +8,15 @@
 import Foundation
 import Day12
 
-let data = try! Data(contentsOf: URL(fileURLWithPath: "../../../../input.txt"))
+let arguments = CommandLine.arguments.count
+if arguments < 2 {
+    print("Usage: \(CommandLine.arguments[0]) <filename>")
+    exit(1)
+}
+
+let filename = CommandLine.arguments[1]
+
+let data = try! Data(contentsOf: URL(fileURLWithPath: filename))
 
 var result = part1(data: data)
 print("\(result)")
